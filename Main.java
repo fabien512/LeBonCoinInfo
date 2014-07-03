e3facc1521f2bc18868beb45fdf1fd21c2bd5b7c
fabien512 <ghrenassia@eisti.eu>
2014-07-03 16:00:08 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ParseLeBonCoin plbc = new ParseLeBonCoin();
+		//plbc.getUrl("http://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/paris/?f=a&th=1&ps=4&pe=31&sqs=1&sqe=16");
+		//plbc.getUrl("http://www.seloger.com/recherche.htm?org=engine&idtt=2&cp=75&idtypebien=1,2&pxmin=100000&pxmax=3000000&surfacemin=20&surfacemax=150&");
+	
+
+	}
+
+}
